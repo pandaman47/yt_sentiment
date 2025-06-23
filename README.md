331697db73ed060a3b16b772ebb9c102d460274f
pandaman47 <psaketh47@gmail.com>
2025-06-23 10:26:34 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # yt_sentiment
+
+#conda create -n venv python==3.13 -y
+
+#conda activate venv
+
+#pip install -r requirements.txt
+
+
+## DVC
+
+#dvc init
+
+#dvc repro
+
+#dvc dag
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='youtube',
+    version='0.0.1',
+    author='saketh',
+    packages=find_packages(),
+    install_requires=[],
+    
+)
